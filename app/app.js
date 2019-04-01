6ed83ffa2c1f361525700f5f57eab7a55b1c25d6
wtitze <w.titze@gmail.com>
2019-04-01 06:46:37 +0000

    aggiunti commenti

--- app/app.js
+++ app/app.js
@@ -1,16 +1,23 @@
-/*eslint-env node*/
+ // moduli utilizzati dal progetto
 
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
+// configurazione motore di render
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-var mysql = require('mysql');
+// gestione della homepage
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+// visualizza l'elenco degli studenti sottoforma di link ipertestuali
+// i dati vengono recuperati da un database MySql
+// e vengono mandati al client utilizzando il template pug presente nel file link.pug
 
 app.get('/link', function (req, res) {
     
@@ -30,6 +37,11 @@ app.get('/link', function (req, res) {
     });
 });
 
+// visualizzazione dei telefoni dello studente selezionato tramite link ipertestuale
+// il link, renderizzato nel file link.pug, è nella forma
+// <site>/dettagli/<id dello studente>. es.: <site>/dettagli/54
+// e viene recuperato utilizzando req.params.ID
+
 app.get('/dettagli/:ID', function (req, res) {
     
     var con = mysql.createConnection({
@@ -47,6 +59,10 @@ app.get('/dettagli/:ID', function (req, res) {
       });
     });
 });
+
+// analogo alla action /link solo che in questo caso si restituisce il file select.pug
+// che visualizza gli studenti in una select (menù a tendina) inserita
+// all'interno di una form, la cui action é /dettaglio
 
 app.get('/select', function (req, res) {
     
@@ -66,6 +82,9 @@ app.get('/select', function (req, res) {
     });
 });
 
+// visualizzazione dei telefoni dello studente selezionato tramite il menù a tendina
+// l'ID dello studente viene recuperato dalla pagine attraverso req.query.studente
+
 app.get('/dettaglio', function (req, res) {
     
     var con = mysql.createConnection({
@@ -83,6 +102,12 @@ app.get('/dettaglio', function (req, res) {
       });
     });
 });
+
+// analogo alla action /link solo che in questo caso si restituisce il file select.pug
+// che visualizza gli studenti in una lista di radio button inseriti
+// all'interno di una form, la cui action é /dettaglio
+// non è necessario codificare una nuova action /dettaglio perché si può utilizzare tranquillamente
+// quella già creata per la select
 
 app.get('/radio', function (req, res) {
     
