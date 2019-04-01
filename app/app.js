2fb5a8f93d46590c84ce3294aa64322cb011e4a4
wtitze <w.titze@gmail.com>
2019-04-01 06:33:19 +0000

    commit iniziale

--- app/app.js
+++ app/app.js
@@ -3,9 +3,105 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+var mysql = require('mysql');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/link', function (req, res) {
+    
+    var con = mysql.createConnection({
+      host: "remotemysql.com",
+      user: "mhJOLDRxsy",
+      password: "trXOgghfWu",
+      database: "mhJOLDRxsy"
+    });
+    
+    con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM studente", function (err, result, fields) {
+        if (err) throw err;
+          res.render("link", {title:"lista studenti", message: "lista degli studenti", list:result});
+      });
+    });
+});
+
+app.get('/dettagli/:ID', function (req, res) {
+    
+    var con = mysql.createConnection({
+      host: "remotemysql.com",
+      user: "mhJOLDRxsy",
+      password: "trXOgghfWu",
+      database: "mhJOLDRxsy"
+    });
+    
+    con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM telefono where IDS=" + req.params.ID, function (err, result, fields) {
+        if (err) throw err;
+          res.render("dettagli", {title:"dettaglio studente", message: "telefoni dello studente selezionato", list:result});
+      });
+    });
+});
+
+app.get('/select', function (req, res) {
+    
+    var con = mysql.createConnection({
+      host: "remotemysql.com",
+      user: "mhJOLDRxsy",
+      password: "trXOgghfWu",
+      database: "mhJOLDRxsy"
+    });
+    
+    con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM studente", function (err, result, fields) {
+        if (err) throw err;
+          res.render("select", {title:"lista studenti", message: "lista degli studenti", list:result});
+      });
+    });
+});
+
+app.get('/dettaglio', function (req, res) {
+    
+    var con = mysql.createConnection({
+      host: "remotemysql.com",
+      user: "mhJOLDRxsy",
+      password: "trXOgghfWu",
+      database: "mhJOLDRxsy"
+    });
+    
+    con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM telefono where IDS=" + req.query.studente, function (err, result, fields) {
+        if (err) throw err;
+          res.render("dettagli", {title:"dettaglio studente", message: "telefoni dello studente selezionato", list:result});
+      });
+    });
+});
+
+app.get('/radio', function (req, res) {
+    
+    var con = mysql.createConnection({
+      host: "remotemysql.com",
+      user: "mhJOLDRxsy",
+      password: "trXOgghfWu",
+      database: "mhJOLDRxsy"
+    });
+    
+    con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM studente", function (err, result, fields) {
+        if (err) throw err;
+          res.render("radio", {title:"lista studenti", message: "lista degli studenti", list:result});
+      });
+    });
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
